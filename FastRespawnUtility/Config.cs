1ca1048fafb657bb8ea202d3794ab72a530c19a0
undid-iridium <francis.x.irizarry@gmail.com>
2022-06-03 03:46:41 -0400

    Config cleanup

--- FastRespawnUtility/Config.cs
+++ FastRespawnUtility/Config.cs
@@ -11,26 +11,31 @@ namespace FastRespawnUtility
     public class Config : IConfig
     {
         /// <inheritdoc />
+        [Description("Whether to enabled or disable plugin")]
         public bool IsEnabled { get; set; } = true;
 
         /// <inheritdoc />
+        [Description("Whether to enabled/disable debug")]
         public bool IsDebugEnabled { get; set; } = false;
 
 
         /// <summary>
         /// Sets the default time to wait to respawn a player. If set to -1.0f then player will not respawn through this plugin.
         /// </summary>
+        [Description("Sets the default time to wait to respawn a player. If set to -1.0f then player will not respawn through this plugin.")]
         public float UniversalRespawnTimer { get; set; } = 5.0f;
 
         /// <summary>
         /// Default role for all classes to respawn as.
         /// </summary>
+        [Description("Default role for all classes to respawn as.")]
         public RoleType UniversalDefaultRole { get; set; } = RoleType.ChaosMarauder;
 
 
         /// <summary>
         /// What to respawn a player as based on their previous role. Probability occurance given by float. If no role can be selected based on probability, default to UniversalDefaultRole
         /// </summary>
+        [Description("What to respawn a player as based on their previous role. Probability occurance given by float. If no role can be selected based on probability, default to UniversalDefaultRole.")]
         public Dictionary<RoleType, Dictionary<RoleType, float>> RespawnRerollTypes { get; set; } = new Dictionary<RoleType, Dictionary<RoleType, float>>
         {
                 { RoleType.FacilityGuard, new Dictionary<RoleType, float>() { { RoleType.FacilityGuard, 0.0f } } },
@@ -63,6 +68,7 @@ namespace FastRespawnUtility
         /// <summary>
         /// Sets how long to wait before respawning role, -1.0f means do not respawn, if not in this list then it defaults to UniversalRespawnTimer value.
         /// </summary>
+        [Description("Sets how long to wait before respawning role, -1.0f means do not respawn, if not in this list then it defaults to UniversalRespawnTimer value.")]
         public Dictionary<RoleType, float> RoleRespawnTime { get; set; } = new Dictionary<RoleType, float>
         {         
                 { RoleType.FacilityGuard,-1.0f},
